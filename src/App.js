a51a7f44f207e845820a556a4c06f94c13c7f108
Yashwant Lande <yashlande@gmail.com>
2022-04-22 11:11:34 +0530

    changed the name to microfrontent

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const ProductApp = React.lazy(() => import("PRODUCT/App"));
 
 const App = () => (
   <div className="shell-app">
-    <h2>Hi from Shell App</h2>
+    <h2>Hi from Microfrontend App</h2>
 
     <React.Suspense fallback="Loading....">
       <ProductApp />
